b26ea6ffa59dc39be8ee45c6c85cd4585172084a
KarishmaSingh750 <karishmasingh30435@gmail.com>
2024-08-16 23:03:22 +0530

    intial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,19 @@
+
+// route.ts
+
+import { google } from "@ai-sdk/google";
+import { type CoreMessage, streamText } from "ai";
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: CoreMessage[] } = await req.json();
+
+  const result = await streamText({
+    model: google("models/gemini-1.5-flash-latest"),
+    system: "You are an expert in organic gardening, providing practical and eco-friendly advice.",
+    messages,
+  });
+
+  return result.toAIStreamResponse();
+}
